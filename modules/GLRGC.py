af91bdc1a8aa2981ec96b003c7ab73a6435ee6dc
Keunho Byeon <bkh5922a@gmail.com>
2024-05-15 02:11:46 +0900

    Temp debugging

--- modules/GLRGC.py
+++ modules/GLRGC.py
@@ -64,22 +64,6 @@ class GLRGC(nn.Module):
             loss_consistency = self.consistency_loss(output, output_ema)
             loss = loss_cross_entropy + self.loss_lambda * (loss_global_relation + loss_local_contrastive + loss_consistency)
 
-            debug = False
-            if torch.isnan(loss_cross_entropy).any() or torch.isinf(loss_cross_entropy).any():
-                print("NaNs or Infs in loss_cross_entropy")
-                debug=True
-            if torch.isnan(loss_local_contrastive).any() or torch.isinf(loss_local_contrastive).any():
-                print("NaNs or Infs in loss_local_contrastive")
-                debug = True
-            if torch.isnan(loss_global_relation).any() or torch.isinf(loss_global_relation).any():
-                print("NaNs or Infs in loss_global_relation")
-                debug = True
-            if torch.isnan(loss_consistency).any() or torch.isinf(loss_consistency).any():
-                print("NaNs or Infs in loss_consistency")
-                debug=True
-            if debug:
-                import pdb; pdb.set_trace()
-
             preds = torch.argmax(output, dim=1)
             preds_ema = torch.argmax(output_ema, dim=1)
 
@@ -128,6 +112,22 @@ class GLRGC(nn.Module):
             loss_global_relation = self.global_relation_loss(features, features_ema)
             loss_consistency = self.consistency_loss(output, output_ema)
             loss = loss_cross_entropy + self.loss_lambda * (loss_global_relation + loss_local_contrastive + loss_consistency)
+
+            debug = False
+            if torch.isnan(loss_cross_entropy).any() or torch.isinf(loss_cross_entropy).any():
+                print("NaNs or Infs in loss_cross_entropy")
+                debug=True
+            if torch.isnan(loss_local_contrastive).any() or torch.isinf(loss_local_contrastive).any():
+                print("NaNs or Infs in loss_local_contrastive")
+                debug = True
+            if torch.isnan(loss_global_relation).any() or torch.isinf(loss_global_relation).any():
+                print("NaNs or Infs in loss_global_relation")
+                debug = True
+            if torch.isnan(loss_consistency).any() or torch.isinf(loss_consistency).any():
+                print("NaNs or Infs in loss_consistency")
+                debug=True
+            if debug:
+                import pdb; pdb.set_trace()
 
             if logger is not None:
                 logger.add_history('total', {'loss': loss.item(), 'loss_CE': loss_cross_entropy.item(), 'loss_local': loss_local_contrastive.item(),
